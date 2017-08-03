52bf41c448a244971dce87b9e3a2be9917f147e3
Natalie Perpepaj <perpepajn25@gmail.com>
2017-08-03 13:32:01 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,10 +15,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each do |item|
-      if cheese_types.include?(item) == true
-          return item
-        end
+  array.find do |item|
+      cheese_types.include?(item)
     end
-  nil
 end
